822dfe553550172d21aed8f850d41f858fffbc90
chenyang <chenyang@Koolearn.com>
2019-10-25 19:56:50 +0800

    zk client api

---
+++ zookeeper/src/main/java/com/learning/opensource/zk/curator/Set_Data_Sample.java
@@ -0,0 +1,41 @@
+package com.learning.opensource.zk.curator;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+
+/**
+ * @author chenyang@koolearn-inc.com
+ * @date 2019/10/25 18:33
+ */
+public class Set_Data_Sample {
+    static String path="/zk-book-curator/c5";
+    static RetryPolicy retryPolicy=new ExponentialBackoffRetry(1000,3);
+
+    static CuratorFramework client= CuratorFrameworkFactory.builder()
+            .connectString("10.155.20.155:2181")
+            .sessionTimeoutMs(5000)
+            .retryPolicy(retryPolicy)
+            .build();
+
+    public static void main(String[] args) throws Exception{
+        client.start();
+        client.create()
+                .creatingParentsIfNeeded()
+                .withMode(CreateMode.EPHEMERAL)
+                .forPath(path,"init".getBytes());
+        Stat stat=new Stat();
+        client.getData().storingStatIn(stat).forPath(path);
+        System.out.println("Success set node for:"+path+",new version:"+client.setData().withVersion(stat.getVersion()).forPath(path,"aaa".getBytes()));
+
+        try {
+            client.setData().withVersion(stat.getVersion()).forPath(path);
+        }catch (Exception e){
+            System.out.println("Fail set node due to "+e.getMessage());
+        }
+        Thread.sleep(Integer.MAX_VALUE);
+    }
+}
